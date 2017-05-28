3e13287e58c11e6102a055f614e974db2beeae63
Angel <angelarrojo@gmail.com>
2017-05-28 21:19:12 +0200

    Add Test Configuration

--- src/test/java/ryanair/flights/infrastructure/repository/rest/RoutesApiClientTest.java
+++ src/test/java/ryanair/flights/infrastructure/repository/rest/RoutesApiClientTest.java
@@ -6,12 +6,20 @@ import static org.hamcrest.Matchers.greaterThan;
 import java.util.List;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import junit.framework.TestCase;
+import ryanair.flights.infrastructure.SampleJettyApplication;
 import ryanair.flights.infrastructure.repository.rest.api.Route;
 
-
-public class RoutesApiClientTest extends UnitTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(SampleJettyApplication.class)
+@WebAppConfiguration
+public class RoutesApiClientTest extends TestCase {
 
     @Autowired
     private RoutesApiClient client;
--- src/test/java/ryanair/flights/infrastructure/repository/rest/SchedulesApiClientTest.java
+++ src/test/java/ryanair/flights/infrastructure/repository/rest/SchedulesApiClientTest.java
@@ -3,11 +3,20 @@ package ryanair.flights.infrastructure.repository.rest;
 import java.time.YearMonth;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 
+import junit.framework.TestCase;
+import ryanair.flights.infrastructure.SampleJettyApplication;
 import ryanair.flights.infrastructure.repository.rest.api.Schedules;
 
-public class SchedulesApiClientTest extends UnitTest {
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(SampleJettyApplication.class)
+@WebAppConfiguration
+public class SchedulesApiClientTest extends TestCase {
 
 	@Autowired
 	private SchedulesApiClient client;
